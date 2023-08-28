da333369c549349ed4a2c3826fa5551da51b1707
ShunyaYamagami <yamagami@naxa.co.jp>
2023-08-29 05:37:48 +0900

    cmd

--- do_memo.sh
+++ do_memo.sh
@@ -1,8 +1,3 @@
 cd ~/lab/gda/da/pytorch-adda
 
-. cmd.sh 0 0 3_5_7  --parent DomainNet  --task true_domains    --tmux DomainNet_ADDA
-. cmd.sh 0 0 6_8  --parent DomainNet  --task true_domains    --tmux DomainNet_ADDA_2
-. cmd.sh 0 0 11_10_9  --parent DomainNet  --task true_domains    --tmux DomainNet_ADDA_reverse
-
-
-. cmd.sh 0 0 2  --parent Office31  --task true_domains    
+. cmd.sh 1 0 4  --parent DomainNet  --task true_domains    --tmux DomainNet_ADDA__GPU1
